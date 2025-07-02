7c2d97cd38b424158833506c4a47495326958a2a
Toviah Moldwin <toviah.moldwin@gmail.com>
2025-07-02 16:37:22 +0300

    added bash

---
+++ run_.synclass.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -p gpu.q
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=synclass
+#SBATCH --output=synclass_%j.out
+
+# Load modules or activate conda environment if needed
+# module load anaconda
+# source activate myenv
+
+cd $HOME/code/SynClass
+python synapse_classifier_resnet.py
